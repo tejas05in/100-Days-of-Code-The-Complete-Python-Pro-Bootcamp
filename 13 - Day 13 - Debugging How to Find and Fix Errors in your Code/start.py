3c15f7c13c2caad9f98b48503396923b8a30829c
Dr.TEJAS.J <tejas05in@yahoo.co.in>
2025-01-10 16:01:36 +0530

    debugging code added

---
+++ 13 - Day 13 - Debugging How to Find and Fix Errors in your Code/start.py
@@ -0,0 +1,79 @@
+############ DEBUGGING#####################
+
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 21):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+
+# # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(0, 5)
+# print(dice_imgs[dice_num])
+
+# # Play Computer
+# year = int(input("What's your year of birth? "))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year >= 1994:
+#   print("You are a Gen Z.")
+
+# # Fix the Errors
+# age = int(input("How old are you? "))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
+
+# # Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# print(pages)
+# word_per_page == int(input("Number of words per page: "))
+# print(word_per_page)
+# total_words = pages * word_per_page
+# print(total_words)
+
+# # Use a Debugger
+# def mutate(a_list):
+#     b_list = []
+#     for item in a_list:
+#         new_item = item * 2
+#         b_list.append(new_item)
+#     print(b_list)
+
+
+# mutate([1, 2, 3, 5, 8, 13])
+
+# number = int(input("Which number do you want to check? "))
+
+# if number % 2 == 0:
+#     print("This is an even number")
+# else:
+#     print("This is an odd number")
+
+# year = int(input("Which year do you want to check? "))
+
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year.")
+#         else:
+#             print("Not leap year")
+#     else:
+#         print("Leap year.")
+# else:
+#     print("Not a leap year")
+
+# Debug fizzbuzz
+
+# for num in range(1,101):
+#     if num % 3 == 0 and num % 5 == 0:
+#         print("FizzBuzz")
+#     elif num % 3 == 0:
+#         print("Fizz")
+#     elif num % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(num)
