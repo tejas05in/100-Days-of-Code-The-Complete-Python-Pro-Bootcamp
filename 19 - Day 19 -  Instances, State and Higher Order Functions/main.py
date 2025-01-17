8a434b67aa62bbe4090f0053223eaaf8014b411b
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2025-01-17 23:53:17 +0530

    free drawing added for turtle

--- 19 - Day 19 -  Instances, State and Higher Order Functions/main.py
+++ 19 - Day 19 -  Instances, State and Higher Order Functions/main.py
@@ -5,7 +5,19 @@ screen = Screen()
 
 def move_forwards():
     tim.forward(10)
+def move_backwards():
+    tim.backward(10)
+def move_left():
+    tim.setheading(tim.heading() + 10)
+def move_right():
+    tim.setheading(tim.heading() - 10)
+def clear():
+    tim.reset()
 
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(key="c", fun=clear)
 screen.exitonclick()
