4d761b36054b9a02539db6f440de795bf1fdf950
Dr.TEJAS.J <tejas05in@yahoo.co.in>
2025-01-25 10:45:02 +0530

    ball added

---
+++ 22 - Day 22 - Build Pong The Famous Arcade Game/main.py
@@ -0,0 +1,33 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+screen.listen()
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+ball = Ball()
+
+game_is_on = True
+while game_is_on:
+    time.sleep(.1)
+    ball.move()
+    screen.update()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+screen.exitonclick()
